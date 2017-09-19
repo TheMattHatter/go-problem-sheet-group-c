2eb2dc2e8770ad0ab19680b1e0d9fbbbcc7c9286
Matthew Shiel <mattshiel@MacBook-Pro.local>
2017-09-19 14:51:01 +0100

    Initial commit

---
+++ 01konnichiwa.go
@@ -0,0 +1,12 @@
+// Author: Matthew Shiel 
+// Date: 19-09-2017
+// Go program example 01: Hello World/Konnichiwa
+// Adapted from https://tour.golang.org/welcome/1
+
+package main // 
+
+import "fmt" // Importing the IO package fmt
+
+func main() {
+	fmt.Println("Hello, 世界") // Printing Konnichiwa with the Println function from the "fmt" package
+}
