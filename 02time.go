e19a7a2c7c236792f09f0d756b07ccad9b925556
Matthew Shiel <mattshiel@il.com>
2017-09-20 14:24:14 +0100

    Commented 02time

--- 02time.go
+++ 02time.go
@@ -1,12 +1,17 @@
+// Author: Matthew Shiel
+// Date: 19-09-2017
+// Go problem 02: Current time
+// Adapted from https://tour.golang.org/welcome/4
+
 package main
 
 import (
-	"fmt"
-	"time"
+	"fmt"  // Importing the IO package fmt
+	"time" // Importing the time package for for measuring and displaying time.
 )
 
 func main() {
-	fmt.Println("Welcome to the playground!")
+	fmt.Println("Welcome to the playground!") // Prints our phrase
 
-	fmt.Println("The time is", time.Now())
+	fmt.Println("The time is", time.Now()) // Call time.Now() to fetch the current date/time
 }
