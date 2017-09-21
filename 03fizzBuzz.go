c2b1aa3a09f53a1e73d296c93b62c9ead70f2c2d
Matthew Shiel <mattshiel@il.com>
2017-09-21 22:13:48 +0100

    Commented and optimised 03fizzBuzz

--- 03fizzBuzz.go
+++ 03fizzBuzz.go
@@ -5,22 +5,22 @@
 
 package main
 
-import "fmt"
+import "fmt" // Imports the IO package
 
 func main() {
-	var fizz string = "Fizz"
-	var buzz string = "Buzz"
-	var fizzBuzz string = "FizzBuzz"
+	var fizz string = "Fizz" // fizz string
+	var buzz string = "Buzz" // buzz string
 
+	// for loop loops 100 times
 	for i := 1; i <= 100; i++ {
 		if i % 3 == 0 && i % 5 == 0 {
-			fmt.Println(fizzBuzz)
+			fmt.Println(fizz + buzz) // Prints FizzBuzz For numbers which are multiples of both three and five
 		} else if i % 3 == 0 {
-			fmt.Println(fizz)
+			fmt.Println(fizz) // Prints fizz for multiples of three
 		} else if i % 5 == 0 {
-			fmt.Println(buzz)
+			fmt.Println(buzz) // Prints buzz for multiples of five
 		} else {
-			fmt.Println(i)
+			fmt.Println(i) // Prints the current number in the loop
 		}
 	}
 }
