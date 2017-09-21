d0a87d32a3189d4abb52cf558bf6ac0db89d0c23
Matthew Shiel <mattshiel@il.com>
2017-09-21 11:58:31 +0100

    Changed order of if statement to fix program output

--- 03fizzBuzz.go
+++ 03fizzBuzz.go
@@ -13,12 +13,12 @@ func main() {
 	var fizzBuzz string = "FizzBuzz"
 
 	for i := 1; i <= 100; i++ {
-		if i%3 == 0 {
-			fmt.Println(fizz)
-		} else if i%5 == 0 {
-			fmt.Println(buzz)
-		} else if i%3 == 0 && i%5 == 0 {
+		if i % 3 == 0 && i % 5 == 0 {
 			fmt.Println(fizzBuzz)
+		} else if i % 3 == 0 {
+			fmt.Println(fizz)
+		} else if i % 5 == 0 {
+			fmt.Println(buzz)
 		} else {
 			fmt.Println(i)
 		}
