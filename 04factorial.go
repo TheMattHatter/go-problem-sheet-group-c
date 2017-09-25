b5735873b5e9c4b383bf55f8f95a99f02b4d592c
Matthew Shiel <mattshiel@il.com>
2017-09-25 23:44:00 +0100

    Added clause for when the user enters 0

--- 04factorial.go
+++ 04factorial.go
@@ -11,23 +11,28 @@ import (
 )
 
 func facDigitSum(x int) int {
-        fact := big.NewInt(1) // Declares a new int64 to hold the larger value
 
-        for i := 1; i <= x; i++ {
-                fact.Mul(fact, big.NewInt(int64(i))) // Calculates factorial of user input by looping mutliplication 'x' times
+        if x == 0 {
+                return -1
+        } else {
+                fact := big.NewInt(1) // Declares a new int64 to hold the larger value
+
+                for i := 1; i <= x; i++ {
+                        fact.Mul(fact, big.NewInt(int64(i))) // Calculates factorial of user input by looping mutliplication 'x' times
+                }
+
+                result := 0 // Holds result of digit sum
+                digit := 0  // Holds the digit
+
+                for i := range fact.String() {
+                        digit, _ = strconv.Atoi(string(fact.String()[i])) // Convert our string/character to an int
+                        result += digit                                   // Calculate result by adding our digits
+                }
+                return result
         }
-
-        result := 0 // Holds result of digit sum
-        digit := 0 // Holds the digit 
-
-        for i := range fact.String() {
-                digit, _ = strconv.Atoi(string(fact.String()[i])) // Convert our string/character to an int
-                result += digit // Calculate result by adding our digits
-        }
-        return result
 }
 
 func main() {
-        var a int = 100 // Value we want the factorial digit sum of
+        var a int = 0             // Value we want the factorial digit sum of
         fmt.Println(facDigitSum(a)) // Calls function and prints answer
 }
