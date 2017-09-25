e6f334a65a0649e7c06b84715afc98e46355e9a2
Matthew Shiel <mattshiel@il.com>
2017-09-25 23:38:00 +0100

    Worked out and added digit sum section using strconv

--- 04factorial.go
+++ 04factorial.go
@@ -1,21 +1,33 @@
 // Author: Matthew Shiel
-// Date: 21-09-2017
+// Date: 25-09-2017
 // Go problem 04: Factorial Digit Sum
 
 package main
 
-import "fmt"
+import (
+        "fmt"      // Imports the IO package
+        "math/big" // Imports the package necessary to declare larger data types
+        "strconv"  // Package for converting digits to strings
+)
 
-func factorial(x int) int {
-	//var a [10]int	
-	sum := 1
-	for i := 1; i <= x; i++ {
-		sum = sum * i
-	}
-	return sum
+func facDigitSum(x int) int {
+        fact := big.NewInt(1) // Declares a new int64 to hold the larger value
+
+        for i := 1; i <= x; i++ {
+                fact.Mul(fact, big.NewInt(int64(i))) // Calculates factorial of user input by looping mutliplication 'x' times
+        }
+
+        result := 0 // Holds result of digit sum
+        digit := 0 // Holds the digit 
+
+        for i := range fact.String() {
+                digit, _ = strconv.Atoi(string(fact.String()[i])) // Convert our string/character to an int
+                result += digit // Calculate result by adding our digits
+        }
+        return result
 }
 
 func main() {
-	var a int = 10
-	fmt.Println(factorial(a))
+        var a int = 100 // Value we want the factorial digit sum of
+        fmt.Println(facDigitSum(a)) // Calls function and prints answer
 }
