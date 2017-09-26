59d88eb7cb33f3e0223f20ac4d44f6a5f9c0c72f
Matthew Shiel <mattshiel@il.com>
2017-09-26 14:16:32 +0100

    Removed unnecessary comments + simplified comments where necessary

--- 04factorial.go
+++ 04factorial.go
@@ -13,26 +13,26 @@ import (
 func facDigitSum(x int) int {
 
         if x == 0 {
-                return -1
+                return -1 // If the user enters 0
         } else {
-                fact := big.NewInt(1) // Declares a new int64 to hold the larger value
+                fact := big.NewInt(1) // int64 to hold the large values
 
                 for i := 1; i <= x; i++ {
                         fact.Mul(fact, big.NewInt(int64(i))) // Calculates factorial of user input by looping mutliplication 'x' times
                 }
 
-                result := 0 // Holds result of digit sum
-                digit := 0  // Holds the digit
+                result := 0 // result of digit sum
+                digit := 0  // individual digit
 
                 for i := range fact.String() {
                         digit, _ = strconv.Atoi(string(fact.String()[i])) // Convert our string/character to an int
-                        result += digit                                   // Calculate result by adding our digits
+                        result += digit                                   // Sum up digit total
                 }
                 return result
         }
 }
 
 func main() {
-        var a int = 0               // Value we want the factorial digit sum of
-        fmt.Println(facDigitSum(a)) // Calls function and prints answer
+        var a int = 100             
+        fmt.Println(facDigitSum(a))
 }
