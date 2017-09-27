92cc1c498f07a009b45a6139005537b178cd66e8
Matthew Shiel <mattshiel@il.com>
2017-09-27 22:45:02 +0100

    Added messages for when guess is too high or low

--- 05guess.go
+++ 05guess.go
@@ -5,28 +5,31 @@
 package main
 
 import (
-        "fmt"
-        "math/rand"
-        "time"
+	"fmt"
+	"math/rand"
+	"time"
 )
 
 func main() {
 
-        rand.Seed(int64(time.Now().Nanosecond())) // Call Seed using nanoseconds for a different random int every execution
-        var randNum = ((rand.Int() % 2) + 1)      // Limit random number to 1000
+	rand.Seed(int64(time.Now().Nanosecond())) // Call Seed using nanoseconds for a different random int every execution
+	var randNum = ((rand.Int() % 10) + 1)     // Limit random number from 0 to 10
 
-        guess := -1 // Start guess at negative to initialise While
+	guess := -1 // Start guess at negative to initialise While
+	count := 0
 
-        for guess != randNum {
-                fmt.Print("Enter your guess: ")
-                fmt.Scanf("%d", &guess)
+	for guess != randNum {
+		fmt.Print("Enter your guess: ")
+		fmt.Scanf("%d", &guess)
 
-                if guess == randNum {
-                        fmt.Printf("Wow congratulations you guessed right! The number is %d \n", randNum)
-                } else {
-                        fmt.Println("Oops try again!")
-                }
-
-        }
-
+		if guess == randNum {
+			fmt.Printf("Wow congratulations you guessed right! The number is %d \n", randNum)
+		} else if guess < randNum {
+			fmt.Println("Nope sorry that guess was too low!")
+		} else if guess > randNum {
+			fmt.Println("Nope sorry that guess was too high!")
+		} else {
+			fmt.Println("Oops try again!")
+		}
+	}
 }
