ba847dd9f763dfbac6ac8d430f57488c46073145
Matthew Shiel <mattshiel@gmail.com>
2017-09-28 11:47:37 +0100

    Completed program, commented and formatted

--- 06largestsmallest.go
+++ 06largestsmallest.go
@@ -5,25 +5,26 @@
 package main
 
 import (
-	"fmt"
+	"fmt" // Import the IO package for printing
 )
 
 func main() {
 	list := [10]int{4, 9, 50, 1, 234, 56, 78, 34, 20, 89} // List of integers
-	var smallest = list[0]
+	var smallest = list[0]                                // Doesn't matter which number we initialise to
 	var largest = list[0]
 
-	for i := range list { 
-			if list[i] < smallest {
-				smallest = list[i]
-			}
+	// Loop through the array
+	for i := range list {
+		if list[i] < smallest {
+			smallest = list[i] // If the number is lesser we set it to the smallest
+		}
 
-			if list[i] > largest {
-				largest = list[i]
-			}
+		if list[i] > largest {
+			largest = list[i] // If the number is greater we set it to the largest
+		}
 	}
 
+	// Print the results
 	fmt.Printf("The smallest number in the list is %d\n", smallest)
 	fmt.Printf("The largest number in the list is %d\n", largest)
-	
 }
