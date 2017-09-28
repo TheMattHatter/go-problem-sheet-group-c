597588b66c479d3c55f48ec68b21c46e4e924c93
Matthew Shiel <mattshiel@gmail.com>
2017-09-28 23:55:15 +0100

    Palindrome now checks for Unicode characters too

--- 07palindrome.go
+++ 07palindrome.go
@@ -1,22 +1,32 @@
 // Author: Matthew Shiel
 // Date: 28-09-2017
 // Go problem 07: Palindrome Tester
-// Adapted from https://play.golang.org/p/AbgW-o-2MK
+// Adapted from https://play.golang.org/p/AbgW-o-2MK and http://www.golangpro.com/2017/04/
 
 package main
 
 import (
-	"fmt"
-	"strings"
+	"fmt"     // IO package
+	"strings" // Strings package to manipulate strings
+	"unicode" // Unicode package to check for Unicode characters
 )
 
 func isPalindrome(input string) bool {
-	// Change to lower case to solve case insensitivity
-	input = strings.ToLower(input)
-	// Loop half the word
-	for i := 0; i < len(input)/2; i++ {
+	input = strings.ToLower(input) // Change to lower case to solve case insensitivity
+
+	var letters []rune // Array to hold runes
+
+	// We don't care about the index so we toss it with the blank identifier
+	for _, r := range input {
+		if unicode.IsLetter(r) {
+			letters = append(letters)
+		}
+	}
+
+	// Loop for half the word
+	for i := range letters {
 		// Compare front letter and corresponding end letter of the string
-		if input[i] != input[len(input)-i-1] {
+		if letters[i] != letters[len(letters)-1-i] {
 			return false
 		}
 	}
@@ -27,6 +37,5 @@ func main() {
 	word := ""
 	fmt.Print("Enter a word to see if it's a palindrome!: ")
 	fmt.Scanf("%s", &word)
-	fmt.Println(isPalindrome(word)) //Check palindrome
-
+	fmt.Println(isPalindrome(word)) // Check palindrome
 }
