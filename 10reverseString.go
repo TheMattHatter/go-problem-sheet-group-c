5818099de90c8d69882de851177edf8d763bf94c
Matthew Shiel <mattshiel@gmail.com>
2017-10-06 00:25:56 +0100

    Improved by using runes for unicode coverage

--- 10reverseString.go
+++ 10reverseString.go
@@ -1,6 +1,7 @@
 // Author: Matthew Shiel
 // Date: 01-10-2017
 // Go problem 10: Reverse String
+// Adapted from https://stackoverflow.com/questions/1752414/how-to-reverse-a-string-in-go
 
 package main
 
@@ -8,17 +9,19 @@ import (
 	"fmt" // Import IO package
 )
 
-func reverse(s string) string {
-	reverse := ""
-	// Loop through the string backwards and store in a string
-    for i := len(s)-1; i >= 0; i-- {
-        reverse += string(s[i])
-    }
-    return reverse 
+func main() {
+	s := "I love data representation and querying" // Word to reverse
+
+	fmt.Printf("The reverse of the String '%s' is '%s'", s, reverse(s)) // Print string in reverse
 }
 
-func main() {
-	s := "I love data querying and representation" // Word to reverse
+func reverse(s string) string {
+	// Assume input is  an ASCII UTF-8 encoded string
+	// create an array of runes and loop for the length of the string/array
+	runes := []rune(s)
+    for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
 
-	fmt.Printf("The reverse of the String '%s' is %s", s, reverse(s)) // Print string in reverse
+        runes[i], runes[j] = runes[j], runes[i]
+    }
+    return string(runes)
 }
